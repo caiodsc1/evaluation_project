5af2637676cbbff029931d2763b800896fef9c2c
caiodsc <caio.dscamara@gmail.com>
2019-12-03 16:23:37 -0300

    Created the evaluation class and its methods

--- lib/evaluation.rb
+++ lib/evaluation.rb
@@ -1,2 +1,24 @@
 class Evaluation
+
+  def build(length = 100)
+
+    raise ArgumentError unless length.is_a?(Integer) && length.positive?
+
+    arr = Array.new(length) { |index| evaluate(index.to_i + 1) }
+    arr.join(', ')
+  end
+
+  private
+
+  def evaluate(number)
+    if number.modulo(35).zero?
+      'Nama Team'
+    elsif number.modulo(7).zero?
+      'Team'
+    elsif number.modulo(5).zero?
+      'Nama'
+    else
+      number.to_s
+    end
+  end
 end
